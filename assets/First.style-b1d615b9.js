0f065a2792f743d7a28348dab964bbf1ff54d8e3
Chernukha21 <Chorniy2008@ukr.net>
2023-10-30 14:19:39 +0200

    Updates

---
+++ assets/First.style-b1d615b9.js
@@ -0,0 +1,13 @@
+import{s as o}from"./index-b79902c1.js";const a=o.table`
+  margin: 0 auto 40px auto;
+  width: 100%;
+  border-collapse: collapse;
+  th {
+    background-color: #f2f2f2; 
+  }
+  th, td {
+    border: 1px solid #ddd; 
+    padding: 8px;
+    text-align: left;
+  }
+`;export{a as T};
